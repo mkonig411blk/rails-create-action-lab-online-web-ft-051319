8d2ecf885061781582c830785302fd645d84ba9e
Margot Konig <margot.konig@blackrock.com>
2019-07-23 01:09:38 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,13 @@ class StudentsController < ApplicationController
   def new
   end
 
-  def create
+   def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
+    # without this redirect, it will try to find create.html.erb
   end
 
 end
